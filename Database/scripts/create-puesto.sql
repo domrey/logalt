7dd723c5c21906b2f93208d78d7bc89cf82f9d24
domrey <jdomrey@gmail.com>
2024-11-16 12:57:54 -0600

    Se añaden los scripts para puestos

---
+++ Database/scripts/create-puesto.sql
@@ -0,0 +1,27 @@
+USE logalt;
+
+CREATE TABLE IF NOT EXISTS RhPuestoTbl (
+	Nivel CHAR(2) NOT NULL COMMENT 'Cadena que representa el nivel del puesto',
+	Familia CHAR(2) NOT NULL COMMENT 'Cadena que representa la familia del puesto',
+	Grupo CHAR(2) NOT NULL COMMENT 'Cadena que representa el rupo de actividad del puesto',
+	Descr VARCHAR(255) NOT NULL COMMENT 'Descripción del puesto de trabajo',
+	Nombre VARCHAR(80) COMMENT 'Descripción corta del puesto',
+	Regimen ENUM('Sindicalizado', 'Confianza') NOT NULL DEFAULT 'Sindicalizado' COMMENT 'Regimen del puesto',
+	APartirDe DATE NOT NULL DEFAULT CURRENT_DATE COMMENT 'Fecha a partir de la cual es vigente el puesto',
+	EsRelevante BOOL NOT NULL DEFAULT 0 COMMENT 'Valor lógico que determina si se trata de un puesto relevante',
+	EsCritico BOOL NOT NULL DEFAULT 0 COMMENT 'Valor lógico que determina si es un puesto crítico',
+	NivelNum TINYINT NOT NULL DEFAULT (Nivel) COMMENT 'Nivel del puesto en formato numérico',
+	Creacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	Id CHAR(8) NOT NULL DEFAULT (CONCAT(Nivel, '.', Familia, '.', Grupo)) COMMENT 'Llave del registro',
+	#PRIMARY KEY (Id)
+	CONSTRAINT PK_Puesto PRIMARY KEY (Nivel, Familia, Grupo)
+)
+ENGINE=InnoDb 
+DEFAULT CHARSET=utf16 COLLATE=utf16_spanish2_ci 
+COMMENT='Tabla de clasiificaciones de puestos de trabajo';
+
+
+
+
+
+
---
+++ Database/scripts/insert-puesto.sql
@@ -0,0 +1,5 @@
+USE logalt;
+
+
+INSERT INTO RhPuestoTbl (Nivel, Familia, Grupo, Descr, Nombre, Regimen, APartirDe, EsRelevante, EsCritico) VALUES ('00', '00', '00', 'Plaza comodín', 'Dummy', 'Sindicalizado', '2015-01-01', 0, 0);
+INSERT INTO RhPuestoTbl (Nivel, Familia, Grupo, Descr, Nombre, Regimen, APartirDe, EsRelevante, EsCritico) VALUES ('08', '44', '06', 'Mensajero', 'Mensajero', 'Sindicalizado', '2015-01-01', 0, 0);
